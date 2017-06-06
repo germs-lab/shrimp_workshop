6246dad7495340d2882d5fbe04eb01e84d83584f
Fan Yang <snisiarc@gmail.com>
2017-06-06 11:08:25 -0500

    finished the post hoc analysis

--- 04_community_variations_and_post_hoc.R
+++ 04_community_variations_and_post_hoc.R
@@ -80,9 +80,44 @@ adonis(rela.dist ~ Culture.media + Variable + as.factor(Replicate), perm=9999,as
 
 ######################################################################
 # post-hoc analysis on significant experimental factors 	    ##
+# using pairwised `adonis`					    ##
 ######################################################################
 # as we can see that "Variable" contributes to the most community variations we are observing (40%). 
 # however, there are 4 different factor levels in "Variable" and we don't know if they are all different from each other or acted differently. 
 
-adonis(rela.dist ~ Variable * Variable, perm=9999,as(sample_data(data.mintax5.excluded.rela.phy),"data.frame"))
+# 1. get the overall metadata for the samples
+si<-data.frame(sample_data(data.mintax5.excluded.rela.phy))
 
+# 2. make sure the experimental factor we would like to evaluate is factor with levels (eg, "Variable" in our case)
+str(si$Variable)
+    #it should look something like this:
+    #> str(si$Variable)
+    # Factor w/ 4 levels "Control","Nutrients",..: 1 3 3 3 3 4 4 4 4 1 ...
+
+# 3. create a dataframe with all pairwised comparisons
+pairs <- t(combn(unique(si$Variable), 2))
+
+# 4. use a for loop:
+df <- data.frame() #start an empty data.frame
+for (i in 1:nrow(pairs)){
+	temp.rowname <- paste(pairs[i, 1], pairs[i, 2], sep="::")
+	temp.phy <- subset_samples(data.mintax5.excluded.rela.phy, Variable %in% pairs[i, ])
+	temp.phy <- prune_taxa(taxa_sums(temp.phy) > 0, temp.phy)
+	temp.dist <- phyloseq::distance(temp.phy, "bray")
+	temp.result <- adonis(temp.dist ~ Variable, perm=9999, as(sample_data(temp.phy), "data.frame"))
+	temp.df <- data.frame(temp.rowname, temp.result$aov.tab[4][1, ], temp.result$aov.tab[5][1, ], temp.result$aov.tab[6][1, ])
+	df <- rbind(df, temp.df)
+}
+names(df)<-c("factor", "rela.F.model", "rela.adonis_R2", "rela.Pr(>F)") #add meaningful columnnames. 
+    #the result looks something like this:
+    #> df
+    #                factor rela.F.model rela.adonis_R2 rela.Pr(>F)
+    #1   Control::Prebiotic     4.293772      0.2347122      0.0041
+    #2          Control::T0     4.613875      0.3157188      0.0046
+    #3   Control::Nutrients    16.057036      0.5008896      0.0001
+    #4        Prebiotic::T0    10.175646      0.5043529      0.0020
+    #5 Prebiotic::Nutrients    15.442680      0.4911375      0.0002
+    #6        T0::Nutrients    29.198579      0.7087278      0.0006
+    ##
+    # the results suggest that all pairs were contributing to the community variations observed significantly. 
+    # and "T0 VS Nutrients" was describing 71% of the community variations. 
