920bf44361b44bd62da021c81f8a865a65ce45f0
Fan Yang <snisiarc@gmail.com>
2017-06-08 16:40:53 -0500

    bar plots with error bars

--- 08_singificant_OTUs_visulization.R
+++ 08_singificant_OTUs_visulization.R
@@ -126,16 +126,142 @@ data.mintax5.excluded.rela.phy <- readRDS("otu_sum_min_5_excluded_4samples_relat
 
 # 2. subset significantly different OTUs from "T0" and "Nutrients" in "Variables"
 # subset "T0" and "Nutrients" first
-t0.nutrient.sig.rela.phy <- subset_samples(data.mintax5.excluded.rela.phy, grepl("T0|Nutrients", Variable))
-t0.nutrient.sig.rela.phy <- prune_taxa(taxa_sums(t0.nutrient.sig.rela.phy) > 0,t0.nutrient.sig.rela.phy) 
-t0.nutrient.sig.rela.phy
+t0.nutrients.sig.rela.phy <- subset_samples(data.mintax5.excluded.rela.phy, grepl("T0|Nutrients", Variable))
+t0.nutrients.sig.rela.phy <- prune_taxa(taxa_sums(t0.nutrient.sig.rela.phy) > 0,t0.nutrient.sig.rela.phy) 
+t0.nutrients.sig.rela.phy
 # subset significant OTUs
-t0.nutrient.sig.rela.phy <- subset_taxa(t0.nutrient.sig.rela.phy, taxa_names(t0.nutrient.sig.rela.phy) %in% t0.nutrients.res.sig$Row.names)
+t0.nutrients.sig.rela.phy <- subset_taxa(t0.nutrient.sig.rela.phy, taxa_names(t0.nutrient.sig.rela.phy) %in% t0.nutrients.res.sig$Row.names)
 # check
-t0.nutrient.sig.rela.phy
-    #> t0.nutrient.sig.rela.phy         
+t0.nutrients.sig.rela.phy
+    #> t0.nutrients.sig.rela.phy         
     #phyloseq-class experiment-level object
     #otu_table()   OTU Table:         [ 680 taxa and 14 samples ]
     #sample_data() Sample Data:       [ 14 samples by 12 sample variables ]
     #tax_table()   Taxonomy Table:    [ 680 taxa by 8 taxonomic ranks ]
 
+# 3. convert the phyloseq object into a 2 dimensional long table
+t0.nutrients.sig.rela.psmelt <- psmelt(t0.nutrients.sig.rela.phy)
+# and the table looks like this:
+head(t0.nutrients.sig.rela.psmelt)
+    #> head(t0.nutrients.sig.rela.psmelt)
+    #         OTU Sample Abundance                      CIBNOR_id Sample_id
+    #124 OTU_1140   lp_7 0.4499120 Sediment_nutrients_72h_E1_rep3     S_300
+    #123 OTU_1140   lp_8 0.4306097 Sediment_nutrients_72h_E1_rep4     S_301
+    #126 OTU_1140  lp_28 0.4108014    Water_nutrients_72h_E1_rep4     S_413
+    #113 OTU_1140   lp_5 0.4070750 Sediment_nutrients_72h_E1_rep1     S_298
+    #117 OTU_1140   lp_6 0.3816208 Sediment_nutrients_72h_E1_rep2     S_299
+    #118 OTU_1140  lp_29 0.3243486    Water_nutrients_72h_E1_rep5     S_414
+    #    Culture.media  Variable Replicate           plate well DNA.concentration
+    #124  Sediment 72h Nutrients         3 Magallon_Plate2   E4              11.5
+    #123  Sediment 72h Nutrients         4 Magallon_Plate2   E5              12.3
+    #126    Water  72h Nutrients         4 Magallon_Plate2   F3               6.6
+    #113  Sediment 72h Nutrients         1 Magallon_Plate2   E2              16.7
+    #117  Sediment 72h Nutrients         2 Magallon_Plate2   E3              15.2
+    #118    Water  72h Nutrients         5 Magallon_Plate2   F4              11.9
+    #    X..of.reads X..of.singletons Experiment SAMPLES    repseq     OTUS   domain
+    #124       22794              667        RK1    lp_7 lp_5|3426 OTU_1140 Bacteria
+    #123       20330              545        RK1    lp_8 lp_5|3426 OTU_1140 Bacteria
+    #126       19078              149        RK1   lp_28 lp_5|3426 OTU_1140 Bacteria
+    #113       19106              703        RK1    lp_5 lp_5|3426 OTU_1140 Bacteria
+    #117       19881              633        RK1    lp_6 lp_5|3426 OTU_1140 Bacteria
+    #118       20053              205        RK1   lp_29 lp_5|3426 OTU_1140 Bacteria
+    #            phylum               class       order       family  genus
+    #124 Proteobacteria Gammaproteobacteria Vibrionales Vibrionaceae Vibrio
+    #123 Proteobacteria Gammaproteobacteria Vibrionales Vibrionaceae Vibrio
+    #126 Proteobacteria Gammaproteobacteria Vibrionales Vibrionaceae Vibrio
+    #113 Proteobacteria Gammaproteobacteria Vibrionales Vibrionaceae Vibrio
+    #117 Proteobacteria Gammaproteobacteria Vibrionales Vibrionaceae Vibrio
+    #118 Proteobacteria Gammaproteobacteria Vibrionales Vibrionaceae Vibrio
+	##
+	# note: the column "Abundance" are the relative abundance of each OTU
+
+# 4. with the above table, we know all OTUs in the table are significantly different between "T0" and "Nutrients". We can do several plots based on this.
+###################################
+# 4.1 top 10 most abundant OTUs   #
+###################################
+	# 4.1.1. identify the total relative abundance of every OTU
+	otu.total <- ddply(t0.nutrients.sig.rela.psmelt, .(OTU), summarise, total = sum(Abundance))
+	head(otu.total)
+        #> head(otu.total)                    
+        #       OTU       total
+        #1    OTU_1 0.025315704
+        #2 OTU_1004 0.006212894
+        #3 OTU_1017 0.007012947
+        #4 OTU_1047 0.001117648
+        #5 OTU_1052 0.013385506
+        #6 OTU_1055 0.012644564
+	# sort by "total" in descending order 
+	otu.total <- otu.total[order(-otu.total$total), ]
+	head(otu.total)
+        #> head(otu.total)                  
+        #         OTU     total
+        #9   OTU_1140 3.5361366
+        #625 OTU_6780 0.8713007
+        #53  OTU_1531 0.7770710
+        #12  OTU_1165 0.5900284
+        #558 OTU_6475 0.3707745
+        #14  OTU_1189 0.3077775
+
+	# find the top 10 most abundant ones
+	otu.top10 <- otu.total[1:10, ]
+	# check
+	otu.top10
+        #> otu.top10
+        #         OTU     total
+        #9   OTU_1140 3.5361366
+        #625 OTU_6780 0.8713007
+        #53  OTU_1531 0.7770710
+        #12  OTU_1165 0.5900284
+        #558 OTU_6475 0.3707745
+        #14  OTU_1189 0.3077775
+        #561 OTU_6478 0.2665647
+        #407  OTU_336 0.2609854
+        #653 OTU_9093 0.2044837
+        #11  OTU_1164 0.1874009
+
+	# 4.1.2. subset these top 10 OTUs out of table "t0.nutrients.sig.rela.psmelt"
+	t0.nutrients.sig.rela.top10 <- subset(t0.nutrients.sig.rela.psmelt, OTU %in% otu.top10$OTU)
+	# check the new table's OTU number
+	length(unique(t0.nutrients.sig.rela.top10$OTU))
+        #> length(unique(t0.nutrients.sig.rela.top10$OTU))
+        #[1] 10
+
+	# 4.1.3. now we can plot
+	p4.1.3.1 <- ggplot(t0.nutrients.sig.rela.top10, aes(x = OTU, y = Abundance, fill=Variable)) + stat_summary(fun.y = mean, geom="bar", position="dodge") + theme_bw() + theme(axis.text.x=element_text(angle=90, hjust = 1, vjust = 0.5))
+	p4.1.3.1
+		# we can see that ggplot2 can calculate average of "Abundance" for each "OTU" across all samples. 
+
+	# let's plot the OTU in descending order (based on their abundance in "Nutrients")
+		# we need a table with unique OTU ids to to this:
+	otu.top10$OTU <- reorder(otu.top10$OTU, -otu.top10$total)
+	# check the levels:
+	str(otu.top10)
+        #> str(otu.top10)                   
+        #'data.frame':   10 obs. of  2 variables:
+        # $ OTU  : Factor w/ 10 levels "OTU_1140","OTU_6780",..: 1 2 3 4 5 6 7 8 9 10
+        #  ..- attr(*, "scores")= num [1:10(1d)] -3.536 -0.187 -0.59 -0.308 -0.777 ...
+        #  .. ..- attr(*, "dimnames")=List of 1
+        #  .. .. ..$ : chr  "OTU_1140" "OTU_1164" "OTU_1165" "OTU_1189" ...
+        # $ total: num  3.536 0.871 0.777 0.59 0.371 ...
+	# reorder the "OTU" levels in "t0.nutrients.sig.rela.top10" based on the "OTU" levels in "otu.top10"
+		# right now, this is what is like
+		str(t0.nutrients.sig.rela.top10$OTU)
+            #> str(t0.nutrients.sig.rela.top10$OTU)
+            # chr [1:140] "OTU_1140" "OTU_1140" "OTU_1140" "OTU_1140" ...
+	t0.nutrients.sig.rela.top10$OTU <- factor(t0.nutrients.sig.rela.top10$OTU, levels=levels(otu.top10$OTU))
+		# now it looks like this:
+		str(t0.nutrients.sig.rela.top10$OTU)
+            #> str(t0.nutrients.sig.rela.top10$OTU)
+            # Factor w/ 10 levels "OTU_1140","OTU_6780",..: 1 1 1 1 1 1 1 1 2 1 ...
+	# replot:
+	p4.1.3.2 <- ggplot(t0.nutrients.sig.rela.top10, aes(x = OTU, y = Abundance, fill=Variable)) + stat_summary(fun.y = mean, geom="bar", position="dodge") + theme_bw() + theme(axis.text.x=element_text(angle=90, hjust = 1, vjust = 0.5))
+	p4.1.3.2
+		# again, average "Abundance" was shown
+
+	# we should add errobars
+	p4.1.3.3 <- p4.1.3.2 + stat_summary(fun.data = mean_se geom="errorbar", position = position_dodge(width = 0.90), width = 0.2)	
+	p4.1.3.3
+	
+	# we can see that some T0 OTUs are really small, we can plot them separately
+	p4.1.3.4 <- p4.1.3.3 + facet_wrap(~Variable, scale="free")
+	p4.1.3.4
